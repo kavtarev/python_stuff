98666cd9387a01dfcc690e66f8450b7086fec5d6
Ivan <ivan.kavtarev@mail.ru>
2021-08-13 23:49:22 +0300

    ff

--- hi.py
+++ hi.py
@@ -1,5 +1,3 @@
-studen = [{input() for j in range(int(input()))} for i in range(int(input()))]
-known_by_everyone = set.intersection(*studen)
-known_by_someone = set.union(*studen)
-print(len(known_by_everyone), *sorted(known_by_everyone), sep='\n')
-print(len(known_by_someone), *sorted(known_by_someone), sep='\n')
+name = input('name: ') # comment input placeholder via :?
+
+print(f"my name is {name}") # f string??
